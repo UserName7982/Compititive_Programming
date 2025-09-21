bb76a11b6354624b8df0fa0e5521af28acaedeed
Harsh <mh116727@gmail.com>
2025-09-21 18:44:36 +0530

    Adding file

---
+++ Graph/Floyedwarshal.java
@@ -0,0 +1,28 @@
+package Graph;
+
+public class Floyedwarshal {
+
+    public int[][] floyedWarshal(int graph[][]) {
+        int n = graph.length;
+        int dist[][] = new int[n][n];
+
+        // Initialize distance array with graph values
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dist[i][j] = graph[i][j];
+            }
+        }
+
+        // Update distances considering each vertex as an intermediate point
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+        return dist;
+    }
+}
