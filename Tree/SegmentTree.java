a2583a9e2d527c9d82f8bef8528f379789626f45
Harsh <mh116727@gmail.com>
2025-11-12 19:29:25 +0530

    Adding File

--- Tree/SegmentTree.java
+++ Tree/SegmentTree.java
@@ -1,19 +1,23 @@
 package Tree;
 
 public class SegmentTree {
+    int[] seg;
+    public SegmentTree(int size) {
+        this.seg=new int[4*size];
+    }
 
-    public void Build(int indx,int low,int high,int l,int r,int[] arr,int seg[]) {
+    public void Build(int indx,int low,int high,int l,int r,int[] arr) {
         if (low==high) {
             seg[indx]=arr[low];
             return;
         }
         int mid=(low+high)/2;
-        Build(2*indx+1,low,mid,l,r,arr,seg);
-        Build(2*indx+2,mid+1,high,l,r,arr,seg);
+        Build(2*indx+1,low,mid,l,r,arr);
+        Build(2*indx+2,mid+1,high,l,r,arr);
         seg[indx]=Math.min(seg[2*indx+1],seg[2*indx+2]);
     }   
 
-    int query(int indx,int low,int high,int l,int r,int seg[]) {
+    int query(int indx,int low,int high,int l,int r) {
         if (r<low || l>high) {
             return Integer.MAX_VALUE;
         }
@@ -21,39 +25,39 @@ public class SegmentTree {
             return seg[indx];
         }
         int mid=(low+high)/2;
-        int left=query(2*indx+1,low,mid,l,r,seg);
-        int right=query(2*indx+2,mid+1,high,l,r,seg);
+        int left=query(2*indx+1,low,mid,l,r);
+        int right=query(2*indx+2,mid+1,high,l,r);
         return Math.min(left,right);
     }
 
-    public int update(int indx,int low,int high,int pos,int val,int seg[]) {
+    public int update(int indx,int low,int high,int pos,int val) {
         if (low==high) {
             seg[indx]=val;
             return val;
         }
         int mid=(low+high)/2;
         if (pos<=mid) {
-            update(2*indx+1,low,mid,pos,val,seg);
+            update(2*indx+1,low,mid,pos,val);
         } else {
-            update(2*indx+2,mid+1,high,pos,val,seg);
+            update(2*indx+2,mid+1,high,pos,val);
         }
         seg[indx]=Math.min(seg[2*indx+1],seg[2*indx+2]);
         return seg[indx];
     }
 
     public static void main(String[] args) {
-        SegmentTree st=new SegmentTree();
+        
         int arr[]={1,3,2,7,9,11};
         int n=arr.length;
-        int seg[]=new int[4*n];
-        st.Build(0,0,n-1,0,n-1,arr,seg);
+        SegmentTree st=new SegmentTree(n);
+        st.Build(0,0,n-1,0,n-1,arr);
         int l=1;
         int r=5;
-        System.out.println(st.query(0,0,n-1,l,r,seg));
+        System.out.println(st.query(0,0,n-1,l,r));
         int pos=1;
         int val=0;
-        st.update(0,0,n-1,pos,val,seg);
+        st.update(0,0,n-1,pos,val);
         arr[pos]=val;
-        System.out.println(st.query(0,0,n-1,l,r,seg));
+        System.out.println(st.query(0,0,n-1,l,r));
     }
 }
