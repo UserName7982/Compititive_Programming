77b453ac873d6c4ba0ce7c8590ad2450589d84d5
UserName7982 <mh116727@gmail.com>
2026-07-15 17:38:12 +0530

    Adding Changes

---
+++ CodeForces/_1200Rated/Three_Activities.java
@@ -0,0 +1,30 @@
+package CodeForces._1200Rated;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Three_Activities {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0) {
+            int n=sc.nextInt();
+            int[][] arr=new int[n][1];
+            int[][] arr1=new int[n][1];
+            int[][] arr2=new int[n][1];
+
+            for(int i=0; i<n; i++){
+                arr[i]=new int[]{sc.nextInt(),i};
+            }
+            for(int i=0; i<n; i++){
+                arr1[i]=new int[]{sc.nextInt(),i};
+            }
+            for(int i=0; i<n; i++){
+                arr2[i]=new int[]{sc.nextInt(),i};
+            }
+            Arrays.sort(arr,(a,b)->b[0]-a[0]);
+            Arrays.sort(arr1,(a,b)->b[0]-a[0]);
+            Arrays.sort(arr2,(a,b)->b[0]-a[0]);
+        }
+    }
+}
